7f573066743e1a65a569d53b0970cd37c3b40266
Diego Landi Ribeiro <33814620+diegolribeiro@users.noreply.github.com>
2019-06-05 21:51:08 -0300

    Add files via upload

---
+++ eclipse-workspace/05_CacheRedis/src/br/com/fiap/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.fiap.entity.Produto;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Long> {
+}
